ea2468942ec3bcf97f1f128938f7560af5a061e5
SGBIT-CC <sgbit@ubuntu.ubuntu-domain>
2018-07-30 03:06:31 -0400

    added file handle to send to new file

--- ipadd.py
+++ ipadd.py
@@ -4,14 +4,12 @@ from bs4 import BeautifulSoup
 url="http://whatismyip.host/"
 data=requests.get(url,headers={'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
 #print(data.content)
-#fhand=open("new.txt","w")
-
-
+fhand=open("new.txt","w")
 soup=BeautifulSoup(data.text, 'html.parser')
 link=soup.find_all(id='ipv4')
-print(link)
-#s=str()
-#fhand.write(s)
+
+s=str(link)
+fhand.write(s)
 #import re
 #for i in cont :
 #    string=re.findall('[0-9][0-9][0-9].+[0-9][0-9][0-9].+[0-9][0-9][0-9].+[0-9][0-9][0-9]',data)
